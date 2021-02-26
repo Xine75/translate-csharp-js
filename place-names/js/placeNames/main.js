6feee700cf6b7f09253ec58821077123683802df
Xine <cjdoza@gmail.com>
2021-02-26 15:44:32 -0600

    fixing push

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,19 @@
 // Put your code here
+
+const placeNames = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+
+console.log("All place names")
+
+for (place of placeNames) {
+    console.log(place);
+}
+
+console.log("'The' Place Names");
+
+const thePlaces = placeNames.filter(n => n.startsWith("The"))
+
+for (place of thePlaces) {
+    console.log(place);
+}
+
